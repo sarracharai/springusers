7cb7b9eab6c0306767bf67b2a270278a12844db8
sarra <sarracharai123@gmail.com>
2023-11-28 23:28:08 +0100

    new commit

---
+++ src/main/java/com/sarra/vols/UsersMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.sarra.vols;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersMicroserviceApplication.class, args);
+	}
+
+}
